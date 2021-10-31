48b7c392938ddc2b10642b7275867966a8ebdf47
JUANCA99COL <jcamilocarorodriguez@gmail.com>
2021-10-31 18:20:08 +1300

    new update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
-console.log('hello');
+// jquey functions 
 
+// jquery functions 
 
-// ==========================================================
-//	Nav Animation
-// ==========================================================
+  console.log("hello world");
 
+  $(".sec-nav").hide();
+  $( document ).ready(function() {
+      console.log( "ready!" );
 
-  /* Open */
-  function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-  }
+  $(".menu-btn").click(function(){
+      // $(".sec-nav").delay( 800 );
+      $(".sec-nav").toggle();
+  });
 
-  /* Close */
-  function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-  }
+  });
 
 
 // ==========================================================
@@ -24,21 +23,18 @@ console.log('hello');
   $(document).ready(function(){
     $(window).scroll(function(){
         if($(window).scrollTop() > 100){
-            $(".navbar").css({"background-color":"white",
+            $(".navbar").css({"background-color":"black",
             
-            
-          });   
+          });  
+
         }
         else{
             $(".navbar").css({"background-color":"",
             
-          
-          
           });
-        }
-
+        }       
+      })
     })
-  })
 
 
 // ==========================================================
@@ -49,6 +45,30 @@ console.log('hello');
   $(window).on("load",function(){
     $(".wrapper-loader").fadeOut("slow");
   });
+
+// ==========================================================
+//	HAMBURGER MENU
+// ==========================================================
+
+
+  const menuBtn = document.querySelector('.menu-btn');
+  let menuOpen = false;
+  menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+      menuBtn.classList.add('open');
+      menuOpen = true;
+    } else {
+      menuBtn.classList.remove('open');
+      menuOpen = false;
+    }
+  });
+
+
+
+
+
+
+
 
 
 
